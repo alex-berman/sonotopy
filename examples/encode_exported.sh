c7858b79bedb2e5501e08b74fcdcdfb1d21047ff
Alex Berman <alex@nimations.com>
2013-09-20 17:50:49 +0200

    encode_exported.sh adjusted for vimeo.com

--- examples/encode_exported.sh
+++ examples/encode_exported.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-avconv -f image2 -r 44100/1024 -i 'export/frame%05d.ppm' -map 0 -i "$1" -r 25 export.mov
+avconv -f image2 -r 44100/1024 -i 'export/frame%05d.ppm' -map 0 -i "$1" -r 30 \
+    -vcodec libx264 -pre:v veryslow \
+    -acodec libvo_aacenc -ab 320k \
+    export.mp4
