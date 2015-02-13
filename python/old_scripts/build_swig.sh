27869c53c3632ebe407c696bc1ced09ce4176177
Peter Bouda <pbouda@cidles.eu>
2015-02-13 15:12:35 +0000

    Deleted old scripts.

--- python/old_scripts/build_swig.sh
+++
@@ -1,3 +0,0 @@
-swig -python -c++ sonotopy.i
-c++ -c sonotopy_wrap.cxx -I /usr/local//Cellar/python/2.7.5/Frameworks/Python.framework/Versions/2.7/include/python2.7
-c++ -shared sonotopy_wrap.o -lpython2.7 -lsonotopy -lfftw3 -L/usr/local//Cellar/python/2.7.5/Frameworks/Python.framework/Versions/2.7/lib -o _sonotopy.so
--- python/old_scripts/build_swig_linux.sh
+++
@@ -1,3 +0,0 @@
-swig -python -c++ sonotopy.i
-g++ -c sonotopy_wrap.cxx -I /usr/include/python2.7 -fPIC
-g++ -shared sonotopy_wrap.o -lpython2.7 -lsonotopy -lfftw3 -L /usr/local/lib -o _sonotopy.so -fPIC
